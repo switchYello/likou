6fc1bc1f051775916bd5214d4380b58b945c9029
switchYello <chaoyuynl@foxmail.com>
2019-12-31 21:51:00 +0800

    提交

--- src/main/java/tree/P687.java
+++ src/main/java/tree/P687.java
@@ -28,6 +28,17 @@ package tree;
 * */
 public class P687 {
 
+    static class A {
+        TreeNode node;
+        int count;
+    }
+
+    private static class MyTreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        int count;
+    }
 
     private static class TreeNode {
         int val;
@@ -40,14 +51,51 @@ public class P687 {
     }
 
     class Solution {
-        int current;
+        int max = 0;
 
         public int longestUnivaluePath(TreeNode root) {
-            current = root.val;
-
-
-            return 0;
+            if (root == null) {
+                return max;
+            }
+            int[] ints = longestUnivaluePathRun(root);
+            max = Math.max(max, ints[0] + ints[1]);
+            return max;
         }
+
+        //得到树的左右两边分量
+        private int[] longestUnivaluePathRun(TreeNode root) {
+            int sum = 0, L = 0, R = 0;
+            if (root.left != null) {
+                int[] left = longestUnivaluePathRun(root.left);
+                L = left[0] + left[1];
+                if (root.left.val == root.val) {
+                    //如果子树左右两边有一个小于1的，则忽略那个小于1的
+                    if (left[0] < 1 || left[1] < 1) {
+                        sum += 1;
+                        sum += Math.max(left[0], left[1]);
+                    } else {
+                        sum += L;
+                    }
+                }
+            }
+
+            if (root.right != null) {
+                int[] right = longestUnivaluePathRun(root.right);
+                R = right[0] + right[1];
+                if (root.right.val == root.val) {
+                    if (right[0] < 1 || right[1] < 1) {
+                        sum += 1;
+                        sum += Math.max(right[0], right[1]);
+                    } else {
+                        sum += R;
+                    }
+                }
+            }
+            max = Math.max(sum, max);
+            return new int[]{L, R};
+        }
+
+
     }
 
 
